84e5210e5a12faade0206c2c8e83ad920e395f60
rupigithub <117827907+rupigithub@users.noreply.github.com>
2024-07-17 23:34:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,10 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  const [currState,setState] = useState();
-  const renderText = ()=>{
-   return  setState("I am learning React. My life is getting better.")
-  }
-  return (
+return (
     <div id="main">
       <h1>React Rendering App</h1>
-      <p><b>{currState}</b></p>
-      <button onClick={renderText}>Click me</button>
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
